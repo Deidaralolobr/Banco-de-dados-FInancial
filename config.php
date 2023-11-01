7d3e6da0edab4f10776a8bed1f704124335a4081
Deidaralolobr <134458149+Deidaralolobr@users.noreply.github.com>
2023-11-01 19:43:29 -0300

    para o junim

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'iandrip123';
+    $dbName = 'db_formulario';
+    
+    $conect = new mysqli ($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    
+?>
